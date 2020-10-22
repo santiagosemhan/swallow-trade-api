1aaa660f62e9b10c07b1d70a092cb3830510208f
Santiago Semhan <santiagosemhan@gmail.com>
2020-10-22 19:20:35 -0300

    fix database.js

--- config/database.js
+++ config/database.js
@@ -1,21 +1,21 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
-    localDefault: {
-      connector: 'mongoose',
-      settings: {
-        host: env('DATABASE_HOST', 'docker.for.mac.host.internal'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'swallow'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'root'),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-    },
+    // localDefault: {
+    //   connector: 'mongoose',
+    //   settings: {
+    //     host: env('DATABASE_HOST', 'docker.for.mac.host.internal'),
+    //     srv: env.bool('DATABASE_SRV', false),
+    //     port: env.int('DATABASE_PORT', 27017),
+    //     database: env('DATABASE_NAME', 'swallow'),
+    //     username: env('DATABASE_USERNAME', 'root'),
+    //     password: env('DATABASE_PASSWORD', 'root'),
+    //   },
+    //   options: {
+    //     authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+    //     ssl: env.bool('DATABASE_SSL', false),
+    //   },
+    // },
     default: {
       connector: 'mongoose',
       settings: {
