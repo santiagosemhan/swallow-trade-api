12c16efccc2480bb9eabc0402d625777ed294e0e
Santiago Semhan <santiagosemhan@gmail.com>
2020-10-22 18:25:39 -0300

    Latest changes to model.

--- config/database.js
+++ config/database.js
@@ -1,30 +1,30 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
-    //   default: {
-    //     connector: 'mongoose',
-    //     settings: {
-    //       host: env('DATABASE_HOST', 'docker.for.mac.host.internal'),
-    //       srv: env.bool('DATABASE_SRV', false),
-    //       port: env.int('DATABASE_PORT', 27017),
-    //       database: env('DATABASE_NAME', 'swallow'),
-    //       username: env('DATABASE_USERNAME', 'root'),
-    //       password: env('DATABASE_PASSWORD', 'root'),
-    //     },
-    //     options: {
-    //       authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-    //       ssl: env.bool('DATABASE_SSL', false),
-    //     },
-    //   },
-    // },
-    default: {
-      connector: 'mongoose',
-      settings: {
-        uri: env('MONGO_DSN', ''),
-      },
-      options: {
-        ssl: env.bool('DATABASE_SSL', false),
+      default: {
+        connector: 'mongoose',
+        settings: {
+          host: env('DATABASE_HOST', 'docker.for.mac.host.internal'),
+          srv: env.bool('DATABASE_SRV', false),
+          port: env.int('DATABASE_PORT', 27017),
+          database: env('DATABASE_NAME', 'swallow'),
+          username: env('DATABASE_USERNAME', 'root'),
+          password: env('DATABASE_PASSWORD', 'root'),
+        },
+        options: {
+          authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
       },
     },
-  },
+    // default: {
+    //   connector: 'mongoose',
+    //   settings: {
+    //     uri: env('MONGO_DSN', ''),
+    //   },
+    //   options: {
+    //     ssl: env.bool('DATABASE_SSL', false),
+    //   },
+    // },
+  // },
 });
