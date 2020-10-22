615155623e8fdf92950f52fec3b6f1f73f934d0c
Santiago Semhan <santiagosemhan@gmail.com>
2020-10-22 10:59:29 -0300

    Update project to connect via mongodb via DSN

--- config/database.js
+++ config/database.js
@@ -1,19 +1,29 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
+    //   default: {
+    //     connector: 'mongoose',
+    //     settings: {
+    //       host: env('DATABASE_HOST', 'docker.for.mac.host.internal'),
+    //       srv: env.bool('DATABASE_SRV', false),
+    //       port: env.int('DATABASE_PORT', 27017),
+    //       database: env('DATABASE_NAME', 'swallow'),
+    //       username: env('DATABASE_USERNAME', 'root'),
+    //       password: env('DATABASE_PASSWORD', 'root'),
+    //     },
+    //     options: {
+    //       authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+    //       ssl: env.bool('DATABASE_SSL', false),
+    //     },
+    //   },
+    // },
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'docker.for.mac.host.internal'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'swallow'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'root'),
+        uri: env('MONGO_DSN', ''),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: true,
       },
     },
   },
