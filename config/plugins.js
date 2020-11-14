384d2b99a58db414921d219bf4c78974ad61b357
Santiago Semhan <santiagosemhan@gmail.com>
2020-11-14 15:01:41 -0300

    Added strapi email plugin.

--- config/plugins.js
+++ config/plugins.js
@@ -2,4 +2,14 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'mongodb',
   },
+  email: {
+    provider: 'sendgrid',
+    providerOptions: {
+      apiKey: env('SENDGRID_API_KEY'),
+    },
+    settings: {
+      defaultFrom: 'app@swallowtrade.com',
+      defaultReplyTo: 'no-reply@swallowtrade.com',
+    },
+  },
 });
