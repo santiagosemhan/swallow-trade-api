f2c0e8ff3ce613fc99c71630285e032f250db9e4
Santiago Semhan <santiagosemhan@gmail.com>
2020-11-14 16:15:50 -0300

    Change host to admin url env var

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   url: env.int('URL'),
   admin: {
-    host: env('ADMIN_HOST'),
+    url: env('ADMIN_URL'),
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'b0686d0ccca28c8683dfd9294f4cff19'),
     },
