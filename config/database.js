db055118265695fa0b84fc075bdb0f0d02f52213
Santiago Semhan <santiagosemhan@gmail.com>
2020-12-17 15:04:34 -0300

    Updating strapi and other fixes

--- config/database.js
+++ config/database.js
@@ -1,29 +1,36 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    // localDefault: {
-    //   connector: 'mongoose',
-    //   settings: {
-    //     host: env('DATABASE_HOST', 'docker.for.mac.host.internal'),
-    //     srv: env.bool('DATABASE_SRV', false),
-    //     port: env.int('DATABASE_PORT', 27017),
-    //     database: env('DATABASE_NAME', 'swallow'),
-    //     username: env('DATABASE_USERNAME', 'root'),
-    //     password: env('DATABASE_PASSWORD', 'root'),
-    //   },
-    //   options: {
-    //     authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-    //     ssl: env.bool('DATABASE_SSL', false),
-    //   },
-    // },
-    default: {
-      connector: 'mongoose',
-      settings: {
-        uri: env('MONGO_DSN', ''),
-      },
-      options: {
-        ssl: env.bool('DATABASE_SSL', false),
-      },
+module.exports = ({ env }) => {
+  let defaultConnection = {
+    connector: 'mongoose',
+    settings: {
+      host: env('DATABASE_HOST', 'docker.for.mac.host.internal'),
+      srv: env.bool('DATABASE_SRV', false),
+      port: env.int('DATABASE_PORT', 27017),
+      database: env('DATABASE_NAME', 'swallow'),
+      username: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'root'),
     },
-  },
-});
+    options: {
+      authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  };
+  if (env('MONGO_DSN')) {
+    defaultConnection = {
+        connector: 'mongoose',
+        settings: {
+          uri: env('MONGO_DSN', ''),
+        },
+        options: {
+          ssl: env.bool('DATABASE_SSL', true),
+        },
+      }
+  }
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: defaultConnection,
+    },
+  };
+};
+
